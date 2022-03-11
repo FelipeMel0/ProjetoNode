d015e3c18c68bbffd5a0c3597b6eb8170b5f2efd
FelipeMel0 <felipedemfernandes@gmail.com>
2022-03-11 08:18:21 -0300

    Testando NodeJs

---
+++ calculadora/calc.js
@@ -0,0 +1,12 @@
+var somaFunc = require("./funcoes/soma")
+var subFunc = require("./funcoes/sub")
+var multFunc = require("./funcoes/mult")
+var divFunc = require("./funcoes/div")
+
+console.log("Soma: " + somaFunc(5, 9))
+
+console.log("Subtração: " + subFunc(100, 10))
+
+console.log("Multiplicação: " + multFunc(5, 9))
+
+console.log("Divisão: " + divFunc(10, 2))
---
+++ nome.js
@@ -0,0 +1,11 @@
+var nome = "Jimmy"
+var sobrenome = "Neutron"
+
+console.log(nome + " " + sobrenome)
+
+var numero1 = 5
+var numero2 = 9
+
+var soma = numero1 + numero2
+
+console.log(soma + " anos")
